14f1b990189a785c1f8e086db982a8fedc12db2d
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-12 14:18:29 +0200

    add address

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product, Employee, Company, To, Address
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Employee)
+admin.site.register(Company)
+admin.site.register(To)
+admin.site.register(Address)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,25 @@
+from importlib.abc import Loader
+from turtle import st
+from unittest import loader
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Address
+from django.template import loader
+
 # Create your views here.
 def index(request):
-    return HttpResponse("good morning")
+    return render(request, "home/address.html")
+
+def addAddress(request):
+    street = request.POST['street']
+    city = request.POST['city']
+    state = request.POST['state']
+    country = request.POST['country']
+    zip = request.POST['zip']
+    template = loader.get_template("home/address.html")
+    new_addr = Address(street=street, city=city, state=state, country=country, zip_code = zip)
+    if(new_addr):
+        new_addr.save()
+        return HttpResponse("Saved successfully")
+    else:
+        return render(request, "home/address.html")
