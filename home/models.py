64d4904f274d06f2bb942099eee5d06205250331
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-12 11:39:13 +0200

    models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from pydoc import describe
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -12,7 +14,32 @@ class Address(models.Model):
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
     company_slogan = models.CharField(max_length=250)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete= models.DO_NOTHING)
     phone_number = models.CharField(max_length = 15)
     email = models.EmailField( max_length=254)
 
+class To(models.Model):
+    recepiant_name = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=16)
+    email = models.EmailField(max_length=254)
+
+class Employee(models.Model):
+    employee_id = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(max_length = 250)
+
+class invoice(models.Model):
+    invoice_number = models.CharField(max_length=9)
+    invoice_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    generated_by = models.CharField(max_length=250)
+    employee_id = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=250)
+    describion = models.CharField(max_length = 500)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
