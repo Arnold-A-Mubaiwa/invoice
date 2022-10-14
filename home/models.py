05d489fea0553eb9d44f1eb88f27c4a54ba7eb1f
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-14 11:57:31 +0200

    model updated

--- home/models.py
+++ home/models.py
@@ -5,11 +5,14 @@ from django.db import models
 
 # Create your models here.
 class Address(models.Model):
-    street = models.CharField(max_length=250)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    country = models.CharField(max_length=5)
-    zip_code = models.CharField(max_length=5)
+    street = models.CharField( max_length=250)
+    city = models.CharField( max_length=50)
+    state = models.CharField( max_length=50)
+    country = models.CharField( max_length=5)
+    zip_code = models.CharField( max_length=5)
+
+    def __str__(self):
+        return self.id
 
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
@@ -18,18 +21,27 @@ class Company(models.Model):
     phone_number = models.CharField(max_length = 15)
     email = models.EmailField( max_length=254)
 
+    def __str__(self):
+        return self.company_name
+
 class To(models.Model):
     recepiant_name = models.CharField(max_length=50)
-    company_name = models.CharField(max_length=50)
+    company_name = models.CharField(blank=False, max_length=50)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=16)
     email = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.company_name
 
 class Employee(models.Model):
     employee_id = models.CharField( max_length=50)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length = 250)
+
+    def __str__(self):
+        return self.name + " " + self.surname
 
 class invoice(models.Model):
     invoice_number = models.CharField(max_length=9)
@@ -38,8 +50,14 @@ class invoice(models.Model):
     generated_by = models.CharField(max_length=250)
     employee_id = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.invoice_number
+
 class Product(models.Model):
     product_id = models.IntegerField()
     product_name = models.CharField(max_length=250)
     describion = models.CharField(max_length = 500)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ from django.template import loader
 # Create your views here.
 def index(request):
     return render(request, "home/address.html")
-
+# adding to database
 def addAddress(request):
     street = request.POST['street']
     city = request.POST['city']
@@ -28,7 +28,13 @@ def addAddress(request):
     template = loader.get_template("home/address.html")
     new_addr = Address(street=street, city=city, state=state, country=country, zip_code = zip)
     if(new_addr):
+        template = loader.get_template("home/success.html")
         new_addr.save()
-        return HttpResponse("Saved successfully")
+        message = "Successful"
+        return HttpResponse(template.render({'message':message}, request))
     else:
-        return render(request, "home/address.html")
+        return HttpResponse(template.render())
+
+# def all_actions(self, request, id):
+#     addAddress()
+#     index()
